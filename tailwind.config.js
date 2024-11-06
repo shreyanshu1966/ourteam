d3a7ccd61a6d7b098757f21c1403d1a6215bf721
Shreyanshu Maske <36789736+shreyanshu1966@users.noreply.github.com>
2024-11-06 18:19:23 +0530

    final animation
    aniamtion working properly

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,13 +11,21 @@ module.exports = {
           '0%': { transform: 'translateY(20px)', opacity: '0' },
           '100%': { transform: 'translateY(0)', opacity: '1' },
         },
+        swipeLeft: {
+          '0%': { transform: 'translateX(100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+        swipeRight: {
+          '0%': { transform: 'translateX(-100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
       },
       animation: {
         riseUpSlow: 'riseUp 1s ease-out',
+        swipeLeft: 'swipeLeft 0.5s ease-out',
+        swipeRight: 'swipeRight 0.5s ease-out',
       },
     },
   },
   plugins: [],
 }
-
-
